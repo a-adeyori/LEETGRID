8cb655e2c5bb0b3622896f55b9e4273c925acbf1
a-adeyori <adeyoriademide@gmail.com>
2024-10-29 13:06:52 -0500

    268 missingNumber

---
+++ 268 missingNumber.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        for i in range(len(nums)+1):
+            if i in nums:
+                continue
+            else :
+                return i
