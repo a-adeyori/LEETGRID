842ffc98a7f402a5476bea7bc233d33800010192
a-adeyori <adeyoriademide@gmail.com>
2024-11-29 05:28:20 -0600

    implemented min/max technique to 387 firstUinqueCharacterInAString

--- srings and hashing/287 firstUniqueCharacterInAString.py
+++ srings and hashing/287 firstUniqueCharacterInAString.py
@@ -1,16 +1,30 @@
 from collections import defaultdict
 class Solution(object):
     def firstUniqChar(self, s):
-        store=defaultdict(int)
-        pos=[]
+        store=defaultdict(list)
 
-        for i in range(len(s)):
-            store[s[i]]+=1
+        for i,n in enumerate(s):
+            store[n].append(i)
+  
+        least = float('inf')
+        for char, idxs in store.items():
+            if len(idxs)==1:
+                least=min(idxs[0],least)
+        return least if least!=float('inf') else -1
 
-        for key,value in store.items():
-            if value==1:
-                pos.append(s.index(key))
+
+
+
+        # store=defaultdict(int)
+        # pos=[]
+
+        # for i in range(len(s)):
+        #     store[s[i]]+=1
+
+        # for key,value in store.items():
+        #     if value==1:
+        #         pos.append(s.index(key))
             
-        least=min(pos) if pos else -1
+        # least=min(pos) if pos else -1
             
-        return least
+        # return least
