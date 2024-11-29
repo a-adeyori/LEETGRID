323464b423419bf6d24e5bbacf195a66b8b1e1bb
a-adeyori <adeyoriademide@gmail.com>
2024-11-29 04:21:53 -0600

    added a maximum technique to maximumNestedParentheses

--- srings and hashing/1614 maximumNestedParentheses.py
+++ srings and hashing/1614 maximumNestedParentheses.py
@@ -1,19 +1,33 @@
 class Solution(object):
     def maxDepth(self, s):
-        stack=[]
-        store=[]
-        count=0
+        maximum = 0
+        count = 0
 
-        if '(' not in s or ')' not in s:
-            return 0
-            
-        for p in s:
-            if stack and p==')' and stack[-1]=='(':
-                store.append(count)
-                stack.pop(-1)
-                count=len(stack)
-                
-            elif p=='(':
-                stack.append(p)
+        for i in s:
+            if i == '(':
                 count+=1
-        return max(store)
+                maximum = max(maximum, count)
+            elif i == ')':
+                count-=1
+        return maximum
+
+        
+        #stack=[]
+        # store=[]
+        # count=0
+
+        # if '(' not in s or ')' not in s:
+        #     return 0
+            
+        # for p in s:
+        #     if stack and p==')' and stack[-1]=='(':
+        #         store.append(count)
+        #         stack.pop(-1)
+        #         count=len(stack)
+                
+        #     elif p=='(':
+        #         stack.append(p)
+        #         count+=1
+        # return max(store)
+
+        
