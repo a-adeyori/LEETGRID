b7673a85d918c207f0d01e3437c7d97800d31208
a-adeyori <adeyoriademide@gmail.com>
2024-12-02 00:04:18 -0600

    figured difference beween and space in the split function argument and leaving it empty

--- srings and hashing/434 numberOfSegmentsInAString.py
+++ srings and hashing/434 numberOfSegmentsInAString.py
@@ -4,12 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-
+        #original train of thought
         # segment=s.split(' ')
         # filters=[]
         # for i in segment:
         #     if i != "":
         #         filters.append(i)
         # return len(filters)
+
+        #realized optimized code
         segment=s.split()
         return len(segment)
