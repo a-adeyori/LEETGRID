25f4fbd02f4b4beb005595833db8428fff3272e0
a-adeyori <adeyoriademide@gmail.com>
2024-12-01 21:54:43 -0600

    added optimal suggestion ffor reverseString

--- arrays and hashing/344 reverseString.py
+++ arrays and hashing/344 reverseString.py
@@ -12,3 +12,19 @@ class Solution(object):
                 s[leftPointer],s[rightPointer]=s[rightPointer],s[leftPointer]
                 leftPointer+=1
                 rightPointer-=1
+
+
+#more optimal according to Claude.AI
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        leftPointer=0
+        rightPointer=len(s)-1
+
+        while leftPointer<rightPointer:
+            s[leftPointer],s[rightPointer]=s[rightPointer],s[leftPointer]
+            leftPointer+=1
+            rightPointer-=1
