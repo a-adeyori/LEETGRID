e8152ea00c842579868e2f9c13748b0bc3f94902
a-adeyori <adeyoriademide@gmail.com>
2024-12-01 21:44:04 -0600

    344 reverseString

---
+++ arrays and hashing/344 reverseString.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        leftPointer=0
+        rightPointer=len(s)-1
+
+        while leftPointer<rightPointer:
+            for i in range(len(s)//2):
+                s[leftPointer],s[rightPointer]=s[rightPointer],s[leftPointer]
+                leftPointer+=1
+                rightPointer-=1
