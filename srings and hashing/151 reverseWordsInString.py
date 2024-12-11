32d5535f8c56e96c6c70ed2d490bd2275208d225
a-adeyori <adeyoriademide@gmail.com>
2024-12-11 08:31:53 -0600

    151 reverseWordsInString

---
+++ srings and hashing/151 reverseWordsInString.py
@@ -0,0 +1,24 @@
+#usig two pointers
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        list_s = s.split(" ")
+        words =[]
+        for i in list_s:
+            if i != "":
+                words.append(i)
+
+        left = 0
+        right = len(words) -1
+
+        print(words)
+
+        while left < right:
+            words[left],words[right] = words[right],words[left]
+            left+=1
+            right-=1
+
+        return " ".join(words)
