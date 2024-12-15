5ecbd78ea8d144204ecfe7c64900054f9e52d2f2
a-adeyori <adeyoriademide@gmail.com>
2024-12-14 19:59:40 -0600

    557 reverseWordsInStringIII

---
+++ srings and hashing/557 reverseWordsInStringIII.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        #brute force
+        def reverse(word):
+            wordList = list(word)
+            left=0
+            right=len(wordList)-1
+            while left<right:
+                wordList[left],wordList[right]=wordList[right],wordList[left]
+                left+=1
+                right-=1
+            return ''.join(wordList)
+
+        wordList = s.split()
+        reverseList = []
+
+        for word in wordList:
+            reverseList.append(reverse(word))
+
+        return " ".join(reverseList)
+
+    print(reverseWords("Let's take LeetCode contest"))
+
+
+        
